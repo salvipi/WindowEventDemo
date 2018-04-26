4d05dca47f6604ef6b5b69c9071ab0bcb6c2170e
salvipi <salvipi@gmail.com>
2018-04-26 11:07:47 +0200

    Versió 0.2 del projecte WindowEventDemo

--- src/windoweventdemo/WindowEventDemo.java
+++ src/windoweventdemo/WindowEventDemo.java
@@ -1,6 +1,7 @@
 package windoweventdemo;
 import java.awt.*;        // using AWT containers and components
 import java.awt.event.*;  // using AWT events and listener interfaces
+import java.net.*;
  
 // An AWT GUI program inherits the top-level container java.awt.Frame
 public class WindowEventDemo extends Frame
